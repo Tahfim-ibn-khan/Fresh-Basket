23c882c5ef0d7d3d97e18d95b2ec9ba649c31aeb
Tahfim Ibn Khan <tahfimibnkhan123@gmail.com>
2025-02-15 01:35:26 +0600

    allowed origin updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  const allowedOrigins ='http://localhost:3001';
+  const allowedOrigins ='http://localhost:3000';
 
   app.enableCors({
     origin: allowedOrigins.split(","),
